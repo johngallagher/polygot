b217f3a36183cb925c167995f8abc81d0ab58a9c
johngallagher <john@synapticmishap.co.uk>
2013-01-31 16:20:46 +0000

    remove PS1 to allow playing well with oh my zsh.

--- git.sh
+++ git.sh
@@ -13,4 +13,5 @@ if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
 fi
 export GIT_PS1_SHOWDIRTYSTATE=true
 
-PS1='\[\033[32m\]$(rvm current)\[\033[00m\]:\[\033[34m\]$(basename $PWD)\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
+# This messes up the prompt with oh my zsh
+#PS1='\[\033[32m\]$(rvm current)\[\033[00m\]:\[\033[34m\]$(basename $PWD)\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
